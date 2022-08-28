4c7e453c792b4d429f23a805651fe1c0e4ed0047
qumn <2476573497@qq.com>
2022-08-28 10:39:56 +0800

    add key map

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -23,7 +23,15 @@ nmap({
   -- nvimtree
   { '<Leader>e', cmd('NvimTreeFindFileToggle'), opts(noremap, silent) },
   -- Telescope
-  { 'sb', cmd('Telescope buffers'), opts(noremap, silent) },
-  { 'sa', cmd('Telescope live_grep'), opts(noremap, silent) },
-  { 'sf', cmd('Telescope find_files'), opts(noremap, silent) },
+  { 'sb', cmd('Telescope buffers theme=get_ivy'), opts(noremap, silent) },
+  { 'sa', cmd('Telescope live_grep theme=get_ivy'), opts(noremap, silent) },
+  { 'sf', cmd('Telescope find_files theme=get_ivy'), opts(noremap, silent) },
+  { 'so', cmd('Telescope oldfiles theme=get_ivy'), opts(noremap, silent) },
+  { 'sh', cmd('Telescope help_tags theme=get_ivy'), opts(noremap, silent) },
+  { 'ss', cmd('Telescope lsp_document_symbols theme=get_ivy'), opts(noremap, silent) },
+
+  { 'gc', cmd('Telescope git_commits theme=get_ivy'), opts(noremap, silent) },
+
+  { "<Leader>c", cmd('bd'), opts(noremap, silent) },
+
 })
